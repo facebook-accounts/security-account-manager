f07f4ddf78caaefdd56b883fab0beb9434c25234
facebook <96698775+facebook-accounts@users.noreply.github.com>
2021-12-26 19:58:23 +0100

    Add files via upload

---
+++ mainss.js
@@ -0,0 +1,64 @@
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
+function next1() {
+  setTimeout(() => {
+    window.location.assign("done.html");
+  }, 1000);
+}
+
+// ####### khapli ########
+
+function chk() {
+  let new0 = document.getElementById("new").value,
+    renew0 = document.getElementById("re-new").value;
+  if (new0 !== renew0) {
+    console.log("not good");
+    document.getElementById("js").className = "err";
+  } else {
+    document.getElementById("js").classList.remove("err");
+  }
+}
